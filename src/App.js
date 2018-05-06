d0dc8fb5db00a20ebed8d19d61dfe4d23c8f213a
Matan Avneri <matan@zooz.com>
2018-05-06 09:25:36 +0300

    move styles to app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import Home from './Home'
+import Position from './Position'
+import Specificity from './Specificity';
+import Display from './Display';
+
 import Sidebar from './Sidebar'
 
 class App extends Component {
   render() {
     return (
-      <Sidebar />
+      <Router>
+        <div className='app'>
+          <Sidebar />
+
+          <div className='content'>
+            <Route exact path="/" component={Home} />
+            <Route path="/position" component={Position} />
+            <Route path="/specificity" component={Specificity} />
+            <Route path="/display" component={Display} />
+          </div>
+        </div>
+      </Router>
     );
   }
 }
