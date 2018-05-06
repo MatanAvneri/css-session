7142c053a9a6a5195bc8e070d62fe927d70fc9fe
Matan Avneri <matan@zooz.com>
2018-05-06 09:21:18 +0300

    add specificity

--- src/Specificity/index.js
+++ src/Specificity/index.js
@@ -1,7 +1,24 @@
-import React from 'react'
+import React, { Fragment } from 'react'
+import './style.css'
+
+const ATTRIBUTE = { test_attr: 'test' }
+const CLASS_NAME = 'test-class'
+const ID = 'testId'
 
 export default function Specificity() {
     return (
-        <div>Specificity</div>
+        <Fragment>
+            <h1>Specificity</h1>
+            <div>I'm a text</div>
+            <div {...ATTRIBUTE}>I'm a text</div>
+            <div className={CLASS_NAME} {...ATTRIBUTE}>I'm a text</div>
+            <div id={ID} className={CLASS_NAME} {...ATTRIBUTE}>I'm a text</div>
+            <div id={ID} className={CLASS_NAME} {...ATTRIBUTE} style={{ color: 'purple' }}>I'm a text</div>
+            <br />
+            <br />
+            <br />
+            <br />
+            <a href="https://specificity.keegan.st/" target="_blank">https://specificity.keegan.st</a>
+        </Fragment>
     )
 }
