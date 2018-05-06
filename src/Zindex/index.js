11646ab3ad1cd93f81bb1e695c8b1084ac228e9a
Matan Avneri <matan@zooz.com>
2018-05-06 10:59:20 +0300

    z index

--- src/Zindex/index.js
+++ src/Zindex/index.js
@@ -5,6 +5,16 @@ export default function Zindex() {
     return (
         <Fragment>
             <h1>Z Index</h1>
+            <div className='z-parent'>
+                parent
+                <div className='z-child1'>
+                    child1
+                </div>
+                <div className='z-child2'>
+                    child2
+                </div>
+            </div>
+            <div className='z-brother'>brother</div>
         </Fragment>
     )
 }
