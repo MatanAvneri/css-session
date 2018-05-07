96f05dad2563ba69f4cbe0aa60269bf5293e01c1
Matan Avneri <matan@zooz.com>
2018-05-07 13:27:41 +0300

    change bad style to dynamicly add rule

--- src/BadStyle/index.js
+++ src/BadStyle/index.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react'
 
 export default class BadStyle extends Component {
-    componentDidMount () {
-        require('./style.css')
+    componentDidMount() {
+        // Grab the first sheet, regardless of media
+        const sheet = document.styleSheets[0];
+        sheet.addRule("*", "color: lightcoral !important;", 1);
     }
     render() {
         return (
