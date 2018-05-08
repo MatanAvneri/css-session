2c6e35496658e975e8686a55b18853aafe5cd3d2
Matan Avneri <matan@zooz.com>
2018-05-08 13:42:47 +0300

    add white space

--- src/Sass/index.js
+++ src/Sass/index.js
@@ -13,6 +13,7 @@ export default function Sass() {
                 <div>2.2</div>
                 <div>2.3</div>
             </div>
+            <div className='sass-text'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus imperdiet enim vulputate dui facilisis ultrices. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aliquam iaculis sed mauris quis gravida. Nulla ut mi non leo lobortis finibus. Duis varius volutpat erat non mattis. Etiam ex diam, congue quis risus tempus, vestibulum scelerisque quam. Integer scelerisque a orci non cursus. Interdum et malesuada fames ac ante ipsum primis in faucibus. In hac habitasse platea dictumst. Proin ullamcorper eleifend turpis sollicitudin ullamcorper. Vestibulum lacinia urna sed sagittis mattis. Maecenas et eros a tellus hendrerit sagittis in quis ante. Phasellus facilisis maximus eleifend. Proin maximus nulla id tempor aliquam.</div>
         </Fragment>
     )
 }
