254b2e3c606211cfe4b3a5ac80727f46e92f289c
Matan Avneri <matan@zooz.com>
2018-05-09 15:28:34 +0300

    add box-sizing

--- src/Sass/index.js
+++ src/Sass/index.js
@@ -15,6 +15,10 @@ export default function Sass() {
             </div>
             <div className='sass-text'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus imperdiet enim vulputate dui facilisis ultrices. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aliquam iaculis sed mauris quis gravida. Nulla ut mi non leo lobortis finibus. Duis varius volutpat erat non mattis. Etiam ex diam, congue quis risus tempus, vestibulum scelerisque quam. Integer scelerisque a orci non cursus. Interdum et malesuada fames ac ante ipsum primis in faucibus. In hac habitasse platea dictumst. Proin ullamcorper eleifend turpis sollicitudin ullamcorper. Vestibulum lacinia urna sed sagittis mattis. Maecenas et eros a tellus hendrerit sagittis in quis ante. Phasellus facilisis maximus eleifend. Proin maximus nulla id tempor aliquam.</div>
             <div className='sass-pseudo' />
+            <div className='sass-box'>
+                <div>border-box</div>
+                <div>content-box</div>
+            </div>
         </Fragment>
     )
 }
